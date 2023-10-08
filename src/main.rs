32189e7a8b2b83a8410bca4e37ef6f54f9e6d2d4
Nyári-Kovács, Dávid Tamás <dev@dnyari.info>
2023-10-08 16:40:17 +0200

    Adjustments to new waveable interafce

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,81 @@
 use sdl2::{self};
-use wavedata;
+use wavedata::{
+    self,
+    units::{Amplitude, Frequency, Proportion, Time},
+};
 
+struct SampleableWrapper<T>(T)
+where
+    T: wavedata::sampling::Sampleable;
 
-struct SampleableWrapper<T>(T) where T: wavedata::sampling::Sampleable + wavedata::waves::Wave;
-
-impl<T> wavedata::sampling::Sampleable for SampleableWrapper<T> 
-    where T: wavedata::sampling::Sampleable + wavedata::waves::Wave {
-    fn sample_into_f32(&self, out: &mut [f32], rate: wavedata::sampling::SamplingRate) -> wavedata::units::Time {
-        self.0.sample_into_f32(out, rate)    
+impl<T> wavedata::sampling::Sampleable for SampleableWrapper<T>
+where
+    T: wavedata::sampling::Sampleable,
+{
+    fn sample_into_f32(&mut self, out: &mut [f32], rate: wavedata::sampling::SamplingRate) {
+        self.0.sample_into_f32(out, rate)
     }
 }
 
-impl<T> sdl2::audio::AudioCallback for SampleableWrapper<T> 
-    where T: wavedata::sampling::Sampleable + wavedata::waves::Wave {
+impl<T> sdl2::audio::AudioCallback for SampleableWrapper<T>
+where
+    T: wavedata::sampling::Sampleable,
+{
     type Channel = f32;
 
     fn callback(&mut self, out: &mut [Self::Channel]) {
-        let offset = self.0.sample_into_f32(out, wavedata::sampling::SamplingRate::new(44100));
-        self.0.shift_mut(offset);
+        self.0
+            .sample_into_f32(out, wavedata::sampling::SamplingRate::new(44100));
     }
 }
 
 fn main() {
-
     let sdl_context = sdl2::init().unwrap();
     let audio_subsystem = sdl_context.audio().unwrap();
 
     let audio_specification = sdl2::audio::AudioSpecDesired {
         freq: Some(44100),
         channels: Some(1),
-        samples: None
+        samples: None,
     };
 
-    let device = audio_subsystem.open_playback(None, &audio_specification, |_spec| {
-        SampleableWrapper(wavedata::waves::Sine::new(
-            wavedata::units::Frequency::new(100.0),
-            wavedata::units::Time::zero(),
-            wavedata::units::Amplitude::new(1.0)
-        ))
-    }).unwrap();
+    let device_constructor = |_spec| {
+        let carrier_signal = wavedata::sampling::WaveSampler::new(wavedata::waves::Sine::new(
+            Frequency::new(512.0),
+            Time::zero(),
+            Amplitude::new(1.0),
+        ));
+        let data_signal = wavedata::sampling::SignalSampler::new(wavedata::signals::am::NRZ::new(
+            wavedata::signals::am::NRZConsts::new(
+                Frequency::new(16.0),
+                Proportion::new(0.1),
+                (Amplitude::new(1.0), Amplitude::new(0.0)),
+            ),
+            wavedata::enc::nrz::Parameters::new(
+                "Nagyon szeretlek angyalom <3 <3 <3"
+                    .as_bytes()
+                    .iter()
+                    .map(|x| x.clone())
+                    .collect(),
+                4,
+            ),
+        ));
+        let composite_sampler = wavedata::sampling::CompositeSampler::new(
+            carrier_signal,
+            data_signal,
+            |input, output| {
+                *output = input.0 * input.1;
+            },
+        );
+
+        SampleableWrapper(composite_sampler)
+    };
+
+    let device = audio_subsystem
+        .open_playback(None, &audio_specification, device_constructor)
+        .unwrap();
 
     device.resume();
 
-    std::thread::sleep(std::time::Duration::from_millis(10000));
-    
+    std::thread::sleep(std::time::Duration::from_millis(20000));
 }
